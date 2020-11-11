c8e606cd965ceac64fd0e489eb884f681240ab31
moriyama14t <moriyama14y@gmail.com>
2020-11-11 22:49:18 +0900

    migration

---
+++ db/migrate/20201111134553_create_medicines.rb
@@ -0,0 +1,10 @@
+class CreateMedicines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :medicines do |t|
+      t.string :Name
+      t.string :Type
+
+      t.timestamps
+    end
+  end
+end
